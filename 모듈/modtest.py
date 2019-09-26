5e41de73481e0808294c4d3a321ef71329b5dea9
dpfladlek <dpfladlek0@daum.net>
2019-09-26 20:58:02 +0900

    과제3

---
+++ 모듈/modtest.py
@@ -0,0 +1,3 @@
+import mod3
+result = mod3.add(3, 4)
+print(result)
---
+++ 모듈/person.py
@@ -0,0 +1,8 @@
+class person:
+    def __init__(self, name, age, address):
+        self.name = name
+        self.age = age
+        self.address = address
+ 
+    def greeting(self):
+        print('내 이름은 '+self.name)
